60b654f78b9dee004adf3280c03c6112a27fa931
JoelVonHelvete <90651319+Joel-A-Navarro@users.noreply.github.com>
2021-12-21 16:34:45 -0600

    hw folders added

--- Homework Instructions/PyBank/main.py
+++ Homework Instructions/PyBank/main.py
@@ -1,28 +1,65 @@
-# test
-import pandas as pd
+# dependencies
+import os
 
-data = pd.read_csv("Resources/budget_data.csv")
-print (data.head())
+# csv data
+path = r'resources\budget_data.csv'
+with open(path) as data:
+    
+    rawData = data.read()
 
-#number of months in data set
-#alternately months = len(data)
-months = data["Date"].count()
-print(months)
+# variables
+month = []
+total_pro = []
+month_change = []
 
-#net total amount of profit/losses
-total = data["Profit/Losses"].sum()
-print(total)
+# import data
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
 
-#calculate changes in profit/losses, find average
-monthly_changes = [0]
-for x in range(months - 1):
-    #(x + 1, 1)
-    current_row = data["Profit/Losses"][x]
-    next_row = data["Profit/Losses"][x + 1]
-    change = next_row - current_row
-    monthly_changes.append(change)
-print(monthly_changes)
-data["monthly_differences"] = monthly_changes
-print(data.head())
-average_change = data["monthly_differences"][1:].mean()
-print(average_change)
+    # total months and profit   
+    for row in csvreader:
+        month.append(str(row[0]))
+        total_pro.append(int(row[1]))
+    total_m = len(month)
+    total_p = sum(totalpro)
+    
+    # month changes     
+    for x in range(len(total_pro)-1):
+        month_change.append(totalpro[x+1]-totalpro[x]) 
+
+    # average monthly change
+    mcpl = sum(month_change)    
+    denom = len(month_change)  
+    average_pl = mcpl/denom
+
+    # maximun and minimum amout
+    greatest = max(month_change)
+    least = min(month_change)
+
+    # index positioning of min and max
+    index = month_change.index(greatest)
+    index_2 = month_change.index(least)
+    month_location1 = (month[index+1])
+    month_location2 = (month[index2+1])
+
+    # print table
+    print("Financial Analysis")
+    print("------------------------------------------")
+    print(f'Total Months: {total_m}')
+    print(f'Total: ${format(total_p,",.0f")}')
+    print(f'Average Change: ${format(average_pl, ",.2f")}')
+    print(f'Greatest Increase in Profits: {month_location1} (${format(greatest, ",.0f")})')
+    print(f'Greatest Decrease in Profits: {(month_location2)} (${format(least,",.0f")})')           
+
+    # create text file
+    os.chdir("C:\\UTSADBC\\python-challenge\\PyBank\\Analysis")
+    f = open("PyBank_output.txt", "a")
+    print("Financial Analysis", file=f)
+    print("------------------------------------------", file=f)
+    print(f'Total Months: {total_m}', file=f)
+    print(f'Total: ${format(total_p,",.0f")}', file=f)
+    print(f'Average Change: ${format(averagepl, ",.2f")}', file=f)
+    print(f'Greatest Increase in Profits: {month_location1} (${format(greatest, ",.0f")})', file=f)
+    print(f'Greatest Decrease in Profits: {(month_location2)} (${format(least,",.0f")})', file=f)  
+    f.close()
